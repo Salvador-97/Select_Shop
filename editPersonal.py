b4efe038a91d0619a0f3339640c495840e51e885
Salvador-97 <salvador.go_97@hotmail.com>
2024-09-10 00:37:16 -0500

    codigo-depurado

--- editPersonal.py
+++ editPersonal.py
@@ -15,13 +15,13 @@ def crearCampo(texto, coordenadaX, coordenadaY):
     return campoAux
 
 def extraerNumero(codigo):
-    
     numerosMarbetes = leerNumerosMarbetes()
     codigoSKU = list(codigo)
     codigoAbreviado = codigoSKU[0] + codigoSKU[1]
     numero = numerosMarbetes[codigoAbreviado]
-    codigoMarbete.append(codigoAbreviado)
-    codigoMarbete.append(numero)
+
+    codigoMarbete[0] = codigoAbreviado
+    codigoMarbete[1] = numero
 
 def obtenerPestañas(pestaña):
     pestañaPersonal = pestaña
@@ -59,10 +59,6 @@ def obtenerPestañas(pestaña):
     separator.place(relx=0, rely=0.5, relwidth=1, relheight=1)
     guardarDatos(campoCodigo, barras, estiba, noProductos, masterPack, descripcion)
 
-def obtenerNuevosDatos():
-    listaDatos()
-
-
 def habilitarEdicion(descripcion, barras, estiba, noProductos, masterPack):
     barras.config(state='enabled')
     descripcion.config(state='enabled')
@@ -93,7 +89,6 @@ def informacionArticulo(listaInfoArticulo, descripcion, barras, estiba, noProduc
     noProductos.insert(0, listaInfoArticulo[3])
     masterPack.insert(0, listaInfoArticulo[4])
     deshabilitarEdicion(descripcion, barras, estiba, noProductos, masterPack)
-    # codigoMarbete = listaInfoArticulo[5]
     
 def guardarDatos(campoCodigo, barras, estiba, noProductos, masterPack, descripcion):
     
@@ -103,7 +98,6 @@ def guardarDatos(campoCodigo, barras, estiba, noProductos, masterPack, descripci
     noTarimas = crearCampo('Numero de tarimas', 0.6, 0.64)
     resto = crearCampo('Resto', 0.2, 0.74)
     
-    
     botonAgregar = ttk.Button(
         pestañaPersonal, 
         text='Generar marbetes', 
@@ -111,43 +105,56 @@ def guardarDatos(campoCodigo, barras, estiba, noProductos, masterPack, descripci
                                     campoCodigo, barras, estiba, noProductos, masterPack, descripcion))
     botonAgregar.place(relx=0.45, rely=0.84)
     
+def validacionDatos(expresionRegular, campoVerificar):
+    campoMayus = campoVerificar.get().upper()
+    resultado = re.findall(expresionRegular, campoMayus)
+    if(resultado):
+        listaDatos.append(campoMayus)
+        validacionCampos = True
+    else:
+        validacionCampos = False
+    return validacionCampos
+
 def editarSheet(contenedor, noTarimas, resto, fecha, proveedor, 
                 campoCodigo, barras, estiba, noProductos, masterPack, descripcion):
-    marbetes = []
-    # scrollPestaña = scroll.ScrolledText()
-    # marbetesGenerados = scroll.ScrolledText(pestañaPersonal, width=50, height=10)
-    # marbetesGenerados.place(relx=0.22, rely=0.90)
     
-    # listaDatos.append(campoCodigo.get())
+    marbetes = []
+    validacionCampos = []
+    
+    listaDatos.append(campoCodigo.get().upper())
     listaDatos.append(estiba.get())
     listaDatos.append(noProductos.get())
     listaDatos.append(descripcion.get())
     listaDatos.append(barras.get())
     listaDatos.append(masterPack.get())
     
-    # listaDatos.append()
-    listaDatos.append(noTarimas.get())
-    listaDatos.append(resto.get())
-    listaDatos.append(fecha.get())
-    listaDatos.append(proveedor.get())
-    listaDatos.append(contenedor.get())
-    # print(listaDatos)
-    # print(noTarimas.get())
-    # marbetesGenerados.grid(column = 0, pady = 10, padx = 10)
-    
-    print(f"Codigo Marbete 1 {codigoMarbete}")
-    
-    for i in range(codigoMarbete[1], codigoMarbete[1] + int(noTarimas.get()), 1):
-        marbetes.append(codigoMarbete[0] + str(i))
-        # marbetesGenerados.tag_config("tag_name", justify='center')
-        # marbetesGenerados.insert(INSERT, codigoMarbete[0] + str(i) + '\n')
-        agregarDatos(codigoMarbete[0] + str(i), listaDatos)
-    messagebox.showinfo("Marbetes", "Marbetes generados exitosamente")
-    print(f"Codigo Marbete 2 {codigoMarbete}")
-    numerosMarbetes = leerNumerosMarbetes()
-    # print(numerosMarbetes)
-    actualizarJSON(numerosMarbetes, codigoMarbete[0], int(noTarimas.get()))
-    codigoMarbete.clear()
+    validacionCampos.append(validacionDatos('[0-9][0-9]*[0-9]*', noTarimas))
+    validacionCampos.append(validacionDatos('[0-9][0-9]*[0-9]*', resto))
+    validacionCampos.append(validacionDatos('(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0,1,2])\/(19|20)\d{2}', fecha))    
+    validacionCampos.append(validacionDatos('[A-Z][A-Z][0-9][0-9]', proveedor))    
+    validacionCampos.append(validacionDatos('[A-Z][A-Z][A-Z][A-Z][0-9][0-9][0-9][0-9][0-9][0-9][0-9]', contenedor))
+
+    if(validacionCampos.count(False) == 0):
+        indiceFinal = codigoMarbete[1] + int(noTarimas.get())
+        for i in range(codigoMarbete[1], indiceFinal, 1):
+            marbetes.append(codigoMarbete[0] + str(i))
+            # if((i == indiceFinal - 1) and (resto.get() != 0)):
+            #     print(listaDatos)
+            #     listaDatos[2] = resto.get()
+            #     print(listaDatos)
+            #     agregarDatos(codigoMarbete[0] + str(i), listaDatos)
+            # else:
+            # marbetesGenerados.tag_config("tag_name", justify='center')
+            # marbetesGenerados.insert(INSERT, codigoMarbete[0] + str(i) + '\n')
+            agregarDatos(codigoMarbete[0] + str(i), listaDatos)
+            
+        messagebox.showinfo("Marbetes", "Marbetes generados exitosamente")
+        codigoMarbete[1] = codigoMarbete[1] + int(noTarimas.get())
+        numerosMarbetes = leerNumerosMarbetes()
+        actualizarJSON(numerosMarbetes, codigoMarbete[0], int(noTarimas.get()))
+        listaDatos.clear()
+    else:
+        messagebox.showerror('Error', 'Dato erróneo, verificar campos')
     
     
 def verificacionInformacionArticulo(campoCodigo, descripcion, barras, estiba, noProductos, masterPack):
@@ -156,28 +163,14 @@ def verificacionInformacionArticulo(campoCodigo, descripcion, barras, estiba, no
     codigoArticulo = codigoArticulo.upper()
     comprobacionRegex = '[A-Z][A-Z][1-9][0-9]*[0-9]*C[1-9][0-9]*'
     resultado = re.findall(comprobacionRegex, codigoArticulo)
+    
     if(resultado):
         
         extraerNumero(codigoArticulo)
 
         if(articulosShop.get(codigoArticulo)):
-            # print('Se encontro la llave')
             listaInfoArticulo = articulosShop.get(codigoArticulo)
             informacionArticulo(listaInfoArticulo, descripcion, barras, estiba, noProductos, masterPack)
-            # print(numerosMarbetes)
-            # codigoProducto = campoCodigo.get()
-            # codigoBarras = barras
-            # cajasTarimas = estiba
-            # productosTarima = noProductos
-            # masterPackCaja = masterPack
-            # descripcionProducto = descripcion
-            listaDatos.append(codigoArticulo)
-            # listaDatos.append(estiba.get())
-            # listaDatos.append(noProductos.get())
-            # listaDatos.append(descripcion.get())
-            # listaDatos.append(barras.get())
-            # listaDatos.append(masterPack.get())
-            # print(listaDatos)
         else:
             messagebox.showwarning("Codigo no encontrado", "No se ha encontrado el producto")
     else:
@@ -202,8 +195,6 @@ def leerNumerosMarbetes():
     return numerosExtraidos
 
 def actualizarJSON(numeros, key, aumento):
-    # print(key)
-    # print(numeros)
     if key in numeros:
         numeros[key] = numeros[key] + aumento
         with open("files/marbetesNumeros.json", "w") as archivoJSONedit:
@@ -212,13 +203,5 @@ def actualizarJSON(numeros, key, aumento):
 
 articulosShop = {}
 listaDatos = []
-codigoMarbete = []
+codigoMarbete = ['', 0]
 pestañaPersonal = None
-# numerosMarbetes = None
-
-codigoProducto = None
-barras = None
-estiba = None
-noProductos = None
-masterPack = None
-descripcion = None
